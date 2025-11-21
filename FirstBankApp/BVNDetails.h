f517ee6fae3153363c923efbe2cd4dee957c3b40
cbc gedu <adekolaadedamola@MacBook-Pro.local>
2025-11-21 17:26:29 +0100

    push codes from device for facial

--- FirstBankApp/BVNDetails.h
+++ FirstBankApp/BVNDetails.h
@@ -7,5 +7,17 @@
 @property (weak, nonatomic) IBOutlet UIButton *continueButton;
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 
+
+//@property (nonatomic, strong) NSString *userid;
+//@property (nonatomic, strong) NSString *regPhoneNumber;
+//@property (nonatomic, strong) NSString *regAccount;
+//@property (nonatomic, strong) NSString *otpReference;
+//
+//@property (nonatomic, strong) NSURLConnection *conn1;
+//@property (nonatomic, strong) NSURLConnection *conn2;
+//@property (nonatomic, strong) NSMutableURLRequest *requested;
+//@property (nonatomic, strong) NSMutableURLRequest *requestOTP;
+//@property (nonatomic, strong) NSMutableData *responseData;
+
 @end
 
